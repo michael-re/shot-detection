a9e082fbf9c62ea9e6e6b8fad40d24baa3e4b14d
michael-re <102764781+michael-re@users.noreply.github.com>
2024-07-06 09:20:30 -0700

    feat: implement shot comparable

--- src/main/java/mre/vsbds/core/shot/Shot.java
+++ src/main/java/mre/vsbds/core/shot/Shot.java
@@ -2,7 +2,7 @@ package mre.vsbds.core.shot;
 
 import mre.vsbds.core.util.Precondition;
 
-public abstract class Shot
+public abstract class Shot implements Comparable<Shot>
 {
     private final int firstFrame;
     private final int lastFrame;
@@ -24,5 +24,16 @@ public abstract class Shot
     public final int lastFrame()
     {
         return lastFrame;
+    }
+
+    @Override
+    public final int compareTo(final Shot other)
+    {
+        final var otherFirstFrame   = (other == null) ? Integer.MAX_VALUE : other.firstFrame;
+        final var otherLastFrame    = (other == null) ? Integer.MAX_VALUE : other.lastFrame;
+        final var firstFrameCompare = Integer.compare(this.firstFrame, otherFirstFrame);
+        return (firstFrameCompare == 0)
+             ? Integer.compare(this.lastFrame, otherLastFrame)
+             : firstFrameCompare;
     }
 }
