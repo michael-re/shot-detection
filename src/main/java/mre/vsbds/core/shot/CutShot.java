cc7f33b96f0669d2512dfcb04bbd986c0c717d0d
michael-re <102764781+michael-re@users.noreply.github.com>
2024-07-06 09:23:46 -0700

    feat: add cut and transition shot specialization

---
+++ src/main/java/mre/vsbds/core/shot/CutShot.java
@@ -0,0 +1,15 @@
+package mre.vsbds.core.shot;
+
+public final class CutShot extends Shot
+{
+    public CutShot(final int firstFrame, final int lastFrame)
+    {
+        super(firstFrame, lastFrame);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "[cs: " + firstFrame() + ", ce: " + lastFrame() + "]";
+    }
+}
---
+++ src/main/java/mre/vsbds/core/shot/TransitionShot.java
@@ -0,0 +1,15 @@
+package mre.vsbds.core.shot;
+
+public class TransitionShot extends Shot
+{
+    public TransitionShot(final int firstFrame, final int lastFrame)
+    {
+        super(firstFrame, lastFrame);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "[fs: " + firstFrame() + ", fe: " + lastFrame() + "]";
+    }
+}
